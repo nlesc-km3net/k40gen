eaad668f307b9cfa4f161ef6e7035d2eb33a5dbe
Roel Aaij <roel.aaij@nikhef.nl>
2019-04-03 11:48:16 +0200

    Fix python test.

--- tests/python/test_extension.py
+++ tests/python/test_extension.py
@@ -1,7 +1,7 @@
 def generate():
     import k40gen as m
     gens = m.Generators(21341, 1245, [7000., 700., 70., 0.])
-    times = m.generate_k40(0, int(1e8), gens, False, "reference")
+    times = m.generate_k40(0, int(1e8), gens, "reference", False)
     assert(times.shape[0] == 4)
 
 
